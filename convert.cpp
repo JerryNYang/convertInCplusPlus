f0246911261b741343ce5ad443b8bf4b05f025d9
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2021-10-09 16:01:35 -0500

    Create convert.cpp

---
+++ convert.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    float kelvin, fahren;
+		cout << "\n\n Convert temper' in Fahrenheit to Kelvin  :\n";
+		cout << endl;	
+    cout << " Input the temper' in Fahrenheit : ";
+    cin >> frh;
+    kel = (5 / 9.0) * (fahren - 32) + 273.15;
+    cout << " The temp in fahrenheit : " << fahren<< endl;
+    cout << " The temp in kelvin : " << kelvin << endl;
+	  cout << endl << endl;
+}
+    return 0;
